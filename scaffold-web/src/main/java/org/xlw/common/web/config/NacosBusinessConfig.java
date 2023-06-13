77e49364c6f2ae5f708116045e0d9b896cec9060
macPro_check_erwan <1076360205@qq.com>
2023-06-13 10:45:38 +0800

    feat: 尝试接触tool

---
+++ scaffold-web/src/main/java/org/xlw/common/web/config/NacosBusinessConfig.java
@@ -0,0 +1,19 @@
+package org.xlw.common.web.config;
+
+import lombok.Data;
+
+/**
+ * Description: check_yo
+ * Author: erwan_check
+ * Email: 1076360205@qq.com
+ * Date: 2023/6/13 09:53
+ */
+@Data
+public class NacosBusinessConfig {
+
+    private String descConfig;
+
+    private Boolean boolConfig;
+
+
+}
