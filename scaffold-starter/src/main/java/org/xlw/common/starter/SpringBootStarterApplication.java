5bc7e69e5de9b0390903c90345bb7056c733bd65
macPro_check_erwan <1076360205@qq.com>
2023-06-13 09:36:35 +0800

    first commit

---
+++ scaffold-starter/src/main/java/org/xlw/common/starter/SpringBootStarterApplication.java
@@ -0,0 +1,17 @@
+package org.xlw.common.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description: check_yo
+ * Author: erwan_check
+ * Email: 1076360205@qq.com
+ * Date: 2023/6/13 09:29
+ */
+@SpringBootApplication
+public class SpringBootStarterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterApplication.class, args);
+    }
+}
