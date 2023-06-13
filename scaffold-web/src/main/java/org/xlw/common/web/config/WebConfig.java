04a5ff38d1472362ae04c3439e33b48713c392b2
macPro_check_erwan <1076360205@qq.com>
2023-06-13 09:46:32 +0800

    web 依赖

---
+++ scaffold-web/src/main/java/org/xlw/common/web/config/WebConfig.java
@@ -0,0 +1,20 @@
+package org.xlw.common.web.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Description: check_yo
+ * Author: erwan_check
+ * Email: 1076360205@qq.com
+ * Date: 2023/6/13 09:39
+ */
+
+@Configuration
+@Slf4j
+@ComponentScan(basePackages = {"org.xlw.common.web"})
+public class WebConfig {
+
+
+}
---
+++ scaffold-web/src/main/java/org/xlw/common/web/controller/TestController.java
@@ -0,0 +1,19 @@
+package org.xlw.common.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description: check_yo
+ * Author: erwan_check
+ * Email: 1076360205@qq.com
+ * Date: 2023/6/13 09:39
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping("/hello")
+    public String test() {
+        return "Test Hello";
+    }
+}
